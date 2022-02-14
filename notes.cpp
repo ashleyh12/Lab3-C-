90dff005e72e2055ea62d3e9b5d73d70d0efcce8
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-02-14 14:21:15 -0500

    Create notes.cpp

---
+++ notes.cpp
@@ -0,0 +1,7 @@
+// composition is when you add the answers to each function call 
+// decomp is how u break them down
+// base case is letting know when to let the recursion stop
+//ex:
+        return num+inclusive(num-1);
+        //num -1 is the decomposition
+        //num + inclusive is the composition
